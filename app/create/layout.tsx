f2b823e8dcdaf15b47ab645be8b9a34b01ceb487
Zeke DuBois <az.duboiz@gmail.com>
2024-05-07 13:54:23 -0700

    fix deploy fail

--- app/create/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default function GameLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-        <main className="flex flex-col items-center justify-center w-full flex-1 px-20">
-            {children}
-        </main>
-        </div>
-    );
-    }
